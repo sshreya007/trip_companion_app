5d4d20fa299554d93165534df1015b252a96be44
sshreya007 <shreyakhadka007@gmail.com>
2025-12-04 20:23:10 +0545

    Added dashboard page

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -5,6 +5,11 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard Page"),
+        backgroundColor: Colors.blue,
+      ),
+    );
   }
 }
