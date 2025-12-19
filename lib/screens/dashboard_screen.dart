aa742bb139b56637dc0f5adcd5fc5cd3476a7064
sshreya007 <shreyakhadka007@gmail.com>
2025-12-19 11:22:04 +0545

    Working on Fonts

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:trip_planner/screens/calendar_screen.dart';
+import 'package:trip_planner/screens/home_screen.dart';
+import 'package:trip_planner/screens/notification_screen.dart';
+import 'package:trip_planner/screens/settings_screen.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = const [
+    HomeScreen(),
+    CalendarScreen(),
+    NotificationScreen(),
+    SettingsScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard Page"),
-        backgroundColor: Colors.blue,
+      appBar: AppBar(),
+
+      body: lstBottomScreen[_selectedIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
