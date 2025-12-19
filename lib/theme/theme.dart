ff6f6e47fff901abb87e58e34762291e440766d4
sshreya007 <shreyakhadka007@gmail.com>
2025-12-19 11:39:02 +0545

    Setting font Open sans Regular

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    fontFamily: 'Open Sans Regular',
+
+    // Global Color Scheme
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFF1BBDB8), // ocean teal
+    ),
+
+    // Bottom Navigation Bar Theme
+
+    // App Bar matches minimalist beach onboarding
+  );
+}
