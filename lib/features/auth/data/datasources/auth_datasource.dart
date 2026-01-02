eff76e895635aad4386ac39c55f12b8d89ed6ef9
sshreya007 <shreyakhadka007@gmail.com>
2026-01-02 17:19:42 +0545

    Working on auth datasource

---
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -0,0 +1,8 @@
+import 'package:trip_planner/features/auth/data/models/auth_hive_model.dart';
+
+abstract interface class IAuthDatasource {
+  Future<bool> register(AuthHiveModel model);
+  Future<AuthHiveModel?> login(String email, String password);
+  Future<AuthHiveModel?> getCurrentUser();
+  Future<bool> logout();
+}
