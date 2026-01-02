05428f90c733fa827972027327449d94d745ba3b
sshreya007 <shreyakhadka007@gmail.com>
2026-01-02 15:28:18 +0545

    Working on auth repository

--- lib/features/auth/domain/entities/auth_entity.dart
+++ lib/features/auth/domain/entities/auth_entity.dart
@@ -4,13 +4,14 @@ class AuthEntity extends Equatable {
   final String? id;
   final String username;
   final String email;
+  final String? password;
 
   const AuthEntity({
     required this.id,
     required this.username,
     required this.email,
+    this.password,
   });
   @override
-  // TODO: implement props
-  List<Object?> get props => [id, username, email];
+  List<Object?> get props => [id, username, email, password];
 }
---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:trip_planner/core/errors/failure.dart';
+import 'package:trip_planner/features/auth/domain/entities/auth_entity.dart';
+
+abstract interface class IAuthRepository {
+  Future<Either<Failure, bool>> register(AuthEntity entity);
+  Future<Either<Failure, AuthEntity>> login(String email, String password);
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
+  Future<Either<Failure, AuthEntity>> logout();
+}
